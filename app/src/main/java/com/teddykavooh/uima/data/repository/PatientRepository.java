e4e870c3f286c0e253d9fba98356c1040654d0e7
Teddykavooh <teddykavooh@gmail.com>
2025-11-05 14:13:11 +0300

    Add Patient functionality

--- app/src/main/java/com/teddykavooh/uima/data/repository/PatientRepository.java
+++ app/src/main/java/com/teddykavooh/uima/data/repository/PatientRepository.java
@@ -1,4 +1,48 @@
 package com.teddykavooh.uima.data.repository;
 
+import com.teddykavooh.uima.data.remote.PatientService;
+import com.teddykavooh.uima.model.Patient;
+import com.teddykavooh.uima.model.PatientRegisterResponse;
+import com.teddykavooh.uima.model.PatientViewResponse;
+import com.teddykavooh.uima.model.PatientsListResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
 public class PatientRepository {
+    private final PatientService patientService;
+
+    public PatientRepository(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    public void getPatient(Patient patient, Callback<PatientViewResponse> callback) {
+        Call<PatientViewResponse> call = patientService.getPatient(patient.getUniqueId());
+        call.enqueue(callback);
+    }
+
+    public void registerPatient(Patient patient, Callback<PatientRegisterResponse> callback) {
+        Call<PatientRegisterResponse> call = patientService.registerPatient(patient);
+        call.enqueue(callback);
+    }
+
+    public void getAllPatients(Patient patient, Callback<PatientsListResponse> callback) {
+        Call<PatientsListResponse> call = patientService.getAllPatients();
+        call.enqueue(callback);
+    }
+
+    // TODO: Add other CRUD operations as needed
+//    public void updatePatient(Patient patient) {
+//        patientService.updatePatient(patient);
+//    }
+//
+//    public void deletePatient(String patientId) {
+//        patientService.deletePatient(patientId);
+//    }
+//
+//    public List<Patient> searchPatients(String query) {
+//        return patientService.searchPatients(query);
+//    }
 }
--- app/src/main/java/com/teddykavooh/uima/model/Patient.java
+++ app/src/main/java/com/teddykavooh/uima/model/Patient.java
@@ -1,4 +1,66 @@
 package com.teddykavooh.uima.model;
 
+import java.text.DateFormat;
+
 public class Patient {
+    private String firstName;
+    private String lastName;
+    private String uniqueId;
+    private DateFormat dob;
+    private String gender;
+    private DateFormat reg_date;
+
+    // Constructor
+    public Patient(String firstName, String lastName, String uniqueId, DateFormat dob, String gender, DateFormat reg_date) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.uniqueId = uniqueId;
+        this.dob = dob;
+        this.gender = gender;
+        this.reg_date = reg_date;
+    }
+
+    // Default Constructor
+    public Patient() {}
+
+    // Getter methods
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getUniqueId() {
+        return uniqueId;
+    }
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public DateFormat getDob() {
+        return dob;
+    }
+    public void setDob(DateFormat dob) {
+        this.dob = dob;
+    }
+
+    public String setGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public DateFormat getReg_date() {
+        return reg_date;
+    }
+    public void setReg_date(DateFormat reg_date) {
+        this.reg_date = reg_date;
+    }
 }
